56e023dfa0397af5f802b476146da0d050b15ceb
Kaleb <rtkaleb@gmail.com>
2025-10-17 10:29:05 -0600

    Adding project

---
+++ order-service/src/test/java/com/nao/retail/orders/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nao.retail.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
