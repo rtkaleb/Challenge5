845423614edbf7b7b04dc1541c6bc113cdd2ef59
Kaleb <rtkaleb@gmail.com>
2025-10-19 20:32:30 -0600

    New feature

--- order-service/src/main/java/com/nao/retail/orders/OrderServiceApplication.java
+++ order-service/src/main/java/com/nao/retail/orders/OrderServiceApplication.java
@@ -3,11 +3,9 @@ package com.nao.retail.orders;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.nao.retail.orders")
 public class OrderServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
 }
